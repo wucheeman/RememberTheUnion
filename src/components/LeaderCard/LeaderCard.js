ad84d2beb3b48814aa86baeb2c4de7433ec219cc
Mark Hainline <wucheeman@gmail.com>
2018-07-04 12:06:47 -0400

    updated so click leadercards if clicked, increment count if not clicked before and reset to 0 if they had already been clicked.

--- src/components/LeaderCard/LeaderCard.js
+++ src/components/LeaderCard/LeaderCard.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./LeaderCard.css";
 
 const FriendCard = props => (
-  <div className="card"  onClick={props.handleIncrement}>
+  // <div className="card"  onClick={props.handleClick}>
+  // <div className="card"  onClick={() => props.handleClick(props.clicked)}>
+  <div className="card"  onClick={() => props.handleClick(props.id)}>
     <div className="img-container">
       <img alt={props.alt} src={props.src} />
     </div>
