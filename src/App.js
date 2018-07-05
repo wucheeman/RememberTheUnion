7ebc05cb16d33eb2ca9c5ee7a12e766e172cfac5
Mark Hainline <wucheeman@gmail.com>
2018-07-05 19:37:41 -0400

    further cosmetic adjustments to the instruction row

--- src/App.js
+++ src/App.js
@@ -78,11 +78,11 @@ class App extends Component {
         />
           <div className='row instructions text-white'>
             <div className='col-sm-3 text-center pt-4 font-weight-bold border-right'>
-              <h4>Instructions</h4>
+              <h4>How to Play</h4>
             </div>
-            <div class='col-sm-1'></div>
-            <div className='col-sm-8'>
-              <h6>Hurrah for the Union is a game of clicks and memory. Win by getting 15 points, earn one point per square when it's clicked once.</h6>
+            {/* <div class='col-sm-1'></div> */}
+            <div className='col-sm-8 pl-4'>
+              <h6>Hurrah for the Union is a game of clicks and memory. Win by getting 15 points, earning one point each time a square is clicked once.</h6>
               <h6>If you click the same square again, you have to start over. To learn more these people, click their names.</h6>
               <h6>Good luck!</h6>
             </div>
