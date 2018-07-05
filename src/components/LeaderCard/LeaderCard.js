21b3ea7a6f14d5ee952e4ec688e1f51c474fe4f0
Mark Hainline <wucheeman@gmail.com>
2018-07-05 13:43:25 -0400

    enhancements to card cosmetics + code cleanup

--- src/components/LeaderCard/LeaderCard.js
+++ src/components/LeaderCard/LeaderCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./LeaderCard.css";
 
-const FriendCard = props => (
+const LeaderCard = props => (
   // <div className="card"  onClick={props.handleClick}>
   // <div className="card"  onClick={() => props.handleClick(props.clicked)}>
   <div className="card"  onClick={() => props.handleClick(props.id)}>
@@ -9,15 +9,11 @@ const FriendCard = props => (
       <img alt={props.alt} src={props.src} />
     </div>
     <div className="content">
-      {/* <ul>
-        <li> */}
-        <a href={props.link} alt={props.alt} target="_blank">
-          {props.alt}
-        </a>
-        {/* </li>
-       </ul> */}
+      <a href={props.link} alt={props.alt} target="_blank">
+        {props.alt}
+      </a>
     </div>
   </div>
 );
 
-export default FriendCard;
+export default LeaderCard;
