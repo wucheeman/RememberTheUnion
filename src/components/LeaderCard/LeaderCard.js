840d9c26084593047005acdd6ce22cf0905b6b8f
Mark Hainline <wucheeman@gmail.com>
2018-07-05 20:56:53 -0400

    removed unused styling

--- src/components/LeaderCard/LeaderCard.js
+++ src/components/LeaderCard/LeaderCard.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./LeaderCard.css";
 
 const LeaderCard = props => (
-  // <div className="card"  onClick={props.handleClick}>
-  // <div className="card"  onClick={() => props.handleClick(props.clicked)}>
   <div className="card"  onClick={() => props.handleClick(props.id)}>
     <div className="img-container">
       <img alt={props.alt} src={props.src} />
